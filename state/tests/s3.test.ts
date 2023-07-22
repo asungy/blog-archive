87112b1b9eb5d74756184610c1ef277f517a9e6a
asungy <62207329+asungy@users.noreply.github.com>
2023-07-21 22:46:08 -0400

    Skip test

--- state/tests/s3.test.ts
+++ state/tests/s3.test.ts
@@ -3,7 +3,7 @@ import { cwd } from "node:process";
 
 const path = require("path");
 
-test("Testing S3Bucket class", async () => {
+test.skip("Create and delete S3 bucket", async () => {
   let bucket = new S3Bucket("bucket-name-4aaa2eaa-b7bf-4d35-aa59-c2a052a98d26");
 
   await bucket.init();
